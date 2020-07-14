1f4d3826ad535981254b4d82701d80297173842e
wanghao <wanghao@tsintergy.com>
2020-07-14 16:53:48 +0800

    dev分支提交

---
+++ src/main/java/com/infinova/sso/git/Check.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C), 2015‐2020, 北京清能互联科技有限公司 Author:  gss Date:  2020/7/14 16:53 History:
+ * <author> <time> <version> <desc>
+ */
+package com.infinova.sso.git;
+
+/**
+ * git分支测试
+ *
+ * @author wangh
+ * @create 2020/7/14
+ * @since 1.0.0
+ */
+public class Check {
+
+}
